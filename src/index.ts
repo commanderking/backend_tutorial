6cb1164f3c9f428c793123695e615aea59228b2b
Jeffrey King <jek582@mail.harvard.edu>
2021-08-08 10:25:11 -0400

    add firebase authentication

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { createConnection, getRepository } from 'typeorm';
+import { createConnection } from 'typeorm';
 import "reflect-metadata";
 import { Activity } from './entities/Activity';
 import express from 'express';
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import {ActivityResolver } from "./resolvers/activity";
+import firebaseAdmin from "firebase-admin";
+
 
 const main = async () => {
      await createConnection({
@@ -14,27 +16,36 @@ const main = async () => {
         synchronize: process.env.NODE_ENV === "development"
     })
 
+    await firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.applicationDefault(),
+         projectId: "reasonloop"
+    });
+
     const app = await express();
-
-
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [ActivityResolver],
             validate: false
         }),
-        context: ({req, res}) => ({ req, res })
+        context: async ({req, res}) => {
+            const token = req.headers.authorization || '';
+            const user = await firebaseAdmin.auth().verifyIdToken(token).catch((error) => {
+                console.log('error', error);
+                return null;
+            })
+        
+            return {
+                req, res, user
+            }
+        },
     })
 
     await apolloServer.start();
-
     apolloServer.applyMiddleware({ app })
-
 
     app.listen(4000, () => {
         console.log('server started on localhost:4000');
     })
-
-
 }
 
 main();
